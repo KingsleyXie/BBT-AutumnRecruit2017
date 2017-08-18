a94452540a2a178ee317c20c38e7f7305095de82
Kingsley <kingsley.xym@gmail.com>
2017-08-18 23:52:54 +0800

    Game Module's PHP Code Done

--- game.php
+++ game.php
@@ -5,14 +5,21 @@ require_once('./config.php');
 existCheck('passedRecord');
 blankCheck('passedRecord');
 
-$sql = '
-UPDATE `game`
-SET
-`passed_number` = `passed_number` + 1
-';
-$stmt = $connect->prepare($sql);
+if (substr($_POST['passedRecord'], 246, 1)) {
+	$stmt = $connect->prepare('
+		UPDATE `game`
+		SET
+		`passed_number` = `passed_number` + 1');
+	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	if (!empty($result)) response(1, '更新数据失败');
+}
+
+$stmt = $connect->prepare('
+	SELECT *
+	FROM `game`');
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if (!empty($result)) response(1, '写入数据库失败');
+if (empty($result)) response(2, '获取数据失败');
 
-response(0);
+echo json_encode(array('code' => 0, 'passedNo' => $result[0]['passed_number']));
