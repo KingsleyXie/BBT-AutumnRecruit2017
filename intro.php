886779b7c916a14c3f1c6285ba491d36005cac5f
Kingsley <kingsley.xym@gmail.com>
2017-08-19 21:35:48 +0800

    Intro Module's PHP Code Done

---
+++ intro.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+require_once('./config.php');
+
+$index = 0;
+
+foreach($connect->query('
+	SELECT *
+	FROM `departments`
+	ORDER BY ID ASC') as $department) {
+	$departments[$index] = array(
+		'id' => (int)$department['ID'],
+		'name' => $department['name'],
+		'img' => $department['img'],
+		'description' => $department['description']);
+	$index++;
+}
+if (empty($departments)) response(1, '获取数据失败，请联系管理员');
+
+echo json_encode(array('code' => 0, 'departments' => $departments));
