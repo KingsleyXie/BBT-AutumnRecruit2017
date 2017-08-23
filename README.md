76bb23dffe6354ba77ffa8ab5df7f83bc4115342
Kingsley <kingsley.xym@gmail.com>
2017-08-23 21:29:00 +0800

    :tada: Make Some Optimizations And Enhancements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BBT-AutumnRecruit2017
 This is the Back-End code for BBT autumn recruit in 2017
 
-**How to use**: Just use SQL code in `initDB.sql` to create the database, and change username & password in `config.php`, then the Back-End Code works and you can dock it with Front-End Code.
+**How to use**: Just use SQL code in `initDB.sql` to create the database, and change username & password in `config.php`, then the Back-End code works and you can dock it with Front-End code.
 
 ***
 
@@ -9,3 +9,4 @@ Extra Information:
 
 - Tag `v0.0.0` is a normal version which includes register module only
 - Tag `v1.0.0` includes departments' data API module and game module(especially write this time as we prepared a game before register module)
+- Tag `v1.1.0` includes some optimizations and enhancements since `v1.0.0`
--- game.php
+++ game.php
@@ -2,20 +2,21 @@
 header('Content-Type: application/json');
 require_once('./config.php');
 
-existCheck('passedRecord');
-blankCheck('passedRecord');
+$request = json_decode(file_get_contents('php://input'), true);
+if (isset($request['isPassed']) && isset($request['passedRecord'])) {
+	if ($request['isPassed'] && substr($request['passedRecord'], 246, 1) === '1')
+		if (!($connect->query('
+			UPDATE `game`
+			SET
+			`passed_number` = `passed_number` + 1')))
+			response(1, '更新数据失败');
 
-if (substr($_POST['passedRecord'], 246, 1) && !($connect->query('
-	UPDATE `game`
-	SET
-	`passed_number` = `passed_number` + 1')))
-	response(1, '更新数据失败');
-
-if (!($connect->query('
-	UPDATE `game`
-	SET
-	`participate_number` = `participate_number` + 1')))
-	response(2, '更新数据失败');
+	if (!($connect->query('
+		UPDATE `game`
+		SET
+		`participate_number` = `participate_number` + 1')))
+		response(2, '更新数据失败');
+}
 
 $stmt = $connect->prepare('
        SELECT passed_number
